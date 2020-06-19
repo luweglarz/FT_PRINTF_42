1f1f6aa7ebf5ecb24a13789177b7252f678e9cac
Lucas WEGLARZ <lweglarz@e3r5p18.42.fr>
2020-06-19 16:55:35 +0200

    printf v 0.00002

--- ft_printf.c
+++ ft_printf.c
@@ -6,119 +6,49 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/10 14:46:41 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/06/17 16:14:00 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/06/19 16:49:33 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stdarg.h>
-#include <unistd.h>
-
-void    ft_putchar(char c)
-{
-    write(1, &c, 1);
-}
-
-void	ft_putstr(char *str)
-{
-	while (*str)
-	{
-		ft_putchar(*str);
-		str++;
-	}
-}
-
-
-void	ft_putnbr(int nb)
-{
-	long	nbr;
-
-	nbr = nb;
-	if (nbr < 0)
-	{
-		ft_putchar('-');
-		nbr = nbr * -1;
-	}
-	if (nbr > 9)
-	{
-		ft_putnbr(nbr / 10);
-		ft_putnbr(nbr % 10);
-	}
-	else
-		ft_putchar(nbr + 48);
-}
-
-
-char    check_convert(const char c)
-{
-    int i;
-    char convert[8] = "cspdiuxX";
-
-    i = 0;
-    while (convert[i])
-    {
-        if (c == convert[i])
-            return (convert[i]);
-        i++;
-    }
-    return (0);
-}
-/*
-void    conv(const char c, va_list args)
-{
-    if (check_convert(c) == 'c')
-        fl_c();
-    else if (check_convert(c) == 's')
-        fl_s();
-    else if (check_convert(c) == 'p')
-        fl_p();
-    else if (check_convert(c) == 'd')
-        fl_d();
-    else if (check_convert(c) == 'i')
-        fl_i();
-    else if (check_convert(c) == 'u')
-        fl_u();
-    else if (check_convert(c) == 'x')
-        fl_x();
-    else    if (check_convert(c) == 'X')
-        fl_X();
-}
-*/
+#include "ft_printf.h"
 
 int		ft_printf(const char *str, ...)
 {
-    int     res;
+    void (*tabFunc[4]) (va_list *) = {conv_c, conv_s, conv_id, conv_id};
+    int index;
+    int i;
+    int res;
     va_list args;
 
     va_start(args, str);
     res = 0;
-    while (*str)
+    index = 0;
+    i = 0;
+    while (str[i])
     {
-        if (*str == '%')
+        if (str[i] == '%')
         {  
-            str++;
-            if (check_convert(*str) == 'c' || check_convert(*str) == 'C')
-                ft_putchar(va_arg(args, int));
-            else if (check_convert(*str) == 'd' || check_convert(*str) == 'i')
-                ft_putnbr(va_arg(args, int));
-            else if (check_convert(*str) == 'p')
-                ft_putaddress(va_arg(args, int));
+            i++;
+            index = check_convert(str[i]);
+            if (index != -1)
+                (*tabFunc[index])(&args);
         }
         else
-        {
-            ft_putchar(*str);
-            res++;
-        }
-    str++;
-    }
+            ft_putchar(str[i]);
+    i++;
+    } 
     va_end(args);
     return (res);
 }
 
+
+#include <stdio.h>
+
 int main()
 {
-    char test;
-    test = 'd';
-    ft_printf("%c", test);
+    char *test;
+    test = "test";
+    ft_printf("%d %d \n",42, 44);
+    //printf("fqwfqw%s", test);
 
 }
