cfb1929e34ca3fa6ef93b23c26aba3897daf5f3e
Lucas WEGLARZ <lweglarz@e3r5p18.42.fr>
2020-06-19 16:55:41 +0200

    printf.h v 0.00002

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/10 16:50:20 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/06/15 14:11:33 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/06/19 16:54:50 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,23 @@
 //{
 
 //}
+
+//struct   t_struct
+//{
+   // int     index;
+   // int     i;
+
+//}        s_struct
+
+int		ft_printf(const char *str, ...);
+void	conv_s(va_list *list);
+void	conv_id(va_list *list);
+void    conv_c(va_list *list);
+int     check_convert(const char c);
+//void    conv(const char c, va_list args);
+void    ft_putchar(char c);
+size_t	ft_strlen(const char *s);
+void	ft_putnbr(int nb);
+
 
 #endif
