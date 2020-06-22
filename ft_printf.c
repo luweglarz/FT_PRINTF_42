c4900f617b386f43835f9010ed938808dd2d847e
Lucas WEGLARZ <lweglarz@e3r11p10.42.fr>
2020-06-22 16:19:32 +0200

    filled tanFunc

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/10 14:46:41 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/06/22 12:55:45 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/06/22 15:40:18 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int		ft_printf(const char *str, ...)
 {
-    void (*tabFunc[8]) (va_list *) = {conv_c, conv_s, conv_p, conv_id, conv_id, 0, conv_x, conv_X};
+    void (*tabFunc[8]) (va_list *) = {conv_c, conv_s, conv_p, conv_id, conv_id, conv_u, conv_x, conv_X};
     int index;
     int i;
     int res;
@@ -26,7 +26,7 @@ int		ft_printf(const char *str, ...)
     i = 0;
     while (str[i])
     {
-        if (str[i] == '%')
+        if (str[i] == '%' && str[i + 1] != '%')
         {  
             i++;
             index = check_convert(str[i]);
@@ -48,7 +48,7 @@ int main()
 {
     char *test;
     test = "test";
-    ft_printf("%x %X %p \n",42, 44, test);
-    printf("%x %X %p \n",42, 44, test);
+    ft_printf("%c %s %p %i%d %u %x %X %% \n", 'c', "test", test, 100, 150, 1421, 42, 44);
+    printf("%c %s %p %i%d %u %x %X %% \n", 'c', "test", test, 100, 150, 1421, 42, 44);
 
 }
