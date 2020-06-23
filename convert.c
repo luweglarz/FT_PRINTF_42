a0e4775f6a4c0a4272ec59bb5988a449ea0f88f9
Lucas WEGLARZ <lweglarz@e3r3p2.42.fr>
2020-06-23 15:15:41 +0200

    need ptr convert

--- convert.c
+++ convert.c
@@ -6,67 +6,67 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 15:09:36 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/06/22 15:33:34 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/06/23 15:06:42 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	conv_s(va_list *list)
+void	conv_s(va_list *list, s_struct *strct)
 {
     char *str;
 
     str = va_arg(*list, char *);
-	write(1, str, ft_strlen(str));
+	ft_putstr(str, strct);
 }
 
-void    conv_c(va_list *list)
+void    conv_c(va_list *list, s_struct *strct)
 {
     char c;
 
     c = va_arg(*list, int);
-    write(1, &c, 1);
+    ft_putchar(c, strct);
 }
 
-void    conv_p(va_list *list)
+void    conv_p(va_list *list, s_struct *strct)
 {
-    int nbr;
+    unsigned int nbr;
 
     nbr = va_arg(*list, int);
-    ft_putptr(nbr);
+    ft_putptr(nbr, strct);
 
 }
 
-void	conv_id(va_list *list)
+void	conv_id(va_list *list, s_struct *strct)
 {
 	int	nbr;
 
 	nbr = va_arg(*list, int);
-	ft_putnbr(nbr);
+	ft_putnbr(nbr, strct);
 }
 
-void    conv_u(va_list *list)
+void    conv_u(va_list *list, s_struct *strct)
 {
     unsigned int nbr;
 
     nbr = va_arg(*list, unsigned int);
-    ft_putunsigned(nbr);
+    ft_putunsigned(nbr, strct);
 }
 
-void    conv_x(va_list *list)
+void    conv_x(va_list *list, s_struct *strct)
 {
     int nbr;
 
     nbr = va_arg(*list, int);
-    ft_puthexax(nbr);
+    ft_puthexax(nbr, strct);
 }
 
-void    conv_X(va_list *list)
+void    conv_X(va_list *list, s_struct *strct)
 {
     int nbr;
 
     nbr = va_arg(*list, int);
-    ft_puthexaX(nbr);
+    ft_puthexaX(nbr, strct);
 }
 
 int    check_convert(const char c)
