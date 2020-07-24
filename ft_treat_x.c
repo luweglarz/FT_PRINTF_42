a0f6211c3ccf5804afd08ac11e8e83b390245f4f
Lucas WEGLARZ <lweglarz@e1r5p8.42.fr>
2020-07-24 15:47:21 +0200

    ft_treat_x.c

--- ft_treat_x.c
+++ ft_treat_x.c
@@ -6,7 +6,7 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/07 14:48:35 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/07/24 15:45:36 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/07/24 15:47:10 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	conv_x(va_list *list, t_struct *strct)
 {
 	unsigned int nbr;
 
-	nbr = va_arg(*list,unsigned long long);
+	nbr = va_arg(*list, unsigned long long);
 	ft_puthexax(nbr, strct);
 }
 
@@ -26,4 +26,4 @@ void	conv_capsx(va_list *list, t_struct *strct)
 
 	nbr = va_arg(*list, unsigned int);
 	ft_puthexcapsx(nbr, strct);
-}	
+}
