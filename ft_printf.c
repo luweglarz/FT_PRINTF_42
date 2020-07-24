5c57056a690b0866aa01f2e640f97f2d73609ae1
Lucas WEGLARZ <lweglarz@e1r5p8.42.fr>
2020-07-24 15:25:05 +0200

    ft_printf.c norme

--- ft_printf.c
+++ ft_printf.c
@@ -6,14 +6,12 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/10 14:46:41 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/07/24 12:09:13 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/07/24 15:24:32 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
-
-// Initialisation de ma structure de tableau de fonction
 
 t_tab		g_tab[9] = {
 	{'%', &conv_per}, {'c', &conv_c }, {'s', &conv_s},
@@ -21,14 +19,12 @@ t_tab		g_tab[9] = {
 	{'u', &conv_u}, {'x', &conv_x}, {'X', &conv_capsx}
 };
 
-// Fonction de parsing des sub-specifiers
-
-void		ft_parse_subspec(const char *str, t_struct *strct, va_list *args)
+void		ft_parse_subsp(const char *str, t_struct *strct, va_list *args)
 {
-		while (str[strct->index])
-		{
+	while (str[strct->index])
+	{
 		if (!ft_isdigit(str[strct->index]) && !ft_is_flag(str[strct->index]))
-			break;
+			break ;
 		if (str[strct->index] == '0' && strct->width == 0 && strct->minus == 0)
 			strct->zero = 1;
 		if (str[strct->index] == '.')
@@ -40,12 +36,10 @@ void		ft_parse_subspec(const char *str, t_struct *strct, va_list *args)
 		if (ft_isdigit(str[strct->index]))
 			conv_flag_digit(str[strct->index], strct);
 		if (ft_isconv(str[strct->index]))
-			break;
+			break ;
 		strct->index++;
-		}
+	}
 }
-
-// Fonction de parsing des caracteres de conversion
 
 void		ft_parse(const char *str, t_struct *strct, va_list *args)
 {
@@ -58,7 +52,7 @@ void		ft_parse(const char *str, t_struct *strct, va_list *args)
 		{
 			tempindex = 0;
 			strct->index++;
-			ft_parse_subspec(str, strct, args);
+			ft_parse_subsp(str, strct, args);
 			while (tempindex <= 9 && str[strct->index] != g_tab[tempindex].conv)
 				tempindex++;
 			if (tempindex < 10)
