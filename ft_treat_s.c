2899ffb7329d80db2981ec0d0ca5357821f260bf
Lucas WEGLARZ <lweglarz@e1r5p8.42.fr>
2020-07-24 15:34:03 +0200

    ft_treat_s.c norme

--- ft_treat_s.c
+++ ft_treat_s.c
@@ -6,7 +6,7 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/07 14:43:34 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/07/23 12:41:04 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/07/24 15:33:47 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 void		conv_s(va_list *list, t_struct *strct)
 {
 	char *str;
+
 	str = va_arg(*list, char *);
 	if (!str)
 		str = "(null)";
 	ft_print_string(str, strct);
-
 }
 
-void		ft_put_minus(char *str, t_struct *strct)
+void		ft_put_minus_string(char *str, t_struct *strct)
 {
 	if (strct->dot == 1)
 		ft_putstrprec(str, strct->prec, strct);
@@ -31,21 +31,16 @@ void		ft_put_minus(char *str, t_struct *strct)
 		ft_putstrprec(str, ft_strlen(str), strct);
 }
 
-// Fonction d'affichage d'une string
-// - Si on un left alignement je gere l'affichage en premier (a gauche)
-// - Si
-
 void		ft_print_string(char *str, t_struct *strct)
 {
 	if (strct->dot == 1 && (size_t)strct->prec > ft_strlen(str))
 		strct->prec = ft_strlen(str);
 	if (strct->minus == 1)
-		ft_put_minus(str, strct);
+		ft_put_minus_string(str, strct);
 	if (strct->dot == 1)
-		ft_putwidth(strct, strct->width, strct->prec);
-	else	
-		ft_putwidth(strct, strct->width, ft_strlen(str));
+		ft_putwidth(strct->width, strct->prec, strct);
+	else
+		ft_putwidth(strct->width, ft_strlen(str), strct);
 	if (strct->minus == 0)
-		ft_put_minus(str, strct);
+		ft_put_minus_string(str, strct);
 }
-
