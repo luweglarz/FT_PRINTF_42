c05df364423c0a54e8092110f800ad5845e7bd42
Lucas WEGLARZ <lweglarz@e1r5p8.42.fr>
2020-07-24 15:41:32 +0200

    utils3.c norme

--- utils3.c
+++ utils3.c
@@ -6,19 +6,19 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 14:43:59 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/07/23 15:15:04 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/07/24 15:41:12 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void		struct_init(t_struct *strct)
+void	struct_init(t_struct *strct)
 {
 	strct->res = 0;
 	strct->index = 0;
-}	
+}
 
-void		flags_init(t_struct *strct)
+void	flags_init(t_struct *strct)
 {
 	strct->prec = 0;
 	strct->width = 0;
@@ -28,21 +28,21 @@ void		flags_init(t_struct *strct)
 	strct->dot = 0;
 }
 
-int			ft_is_flag(int c)
+int		ft_is_flag(int c)
 {
-	char 	*flags;
+	char	*flags;
 	int		i;
 
 	i = 0;
 	flags = "-0.*";
 	while (flags[i])
 	{
-		if(c == flags[i])
+		if (c == flags[i])
 			return (1);
 		i++;
 	}
 	return (0);
-}	
+}
 
 int		ft_isconv(int c)
 {
