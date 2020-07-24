2645a17abc44400fe22cdc0429b5cf3fdbc370d2
Lucas WEGLARZ <lweglarz@e1r5p8.42.fr>
2020-07-24 15:46:40 +0200

    file renamed again

--- ft_treat_x.c
+++ ft_treat_x.c
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_treat_xX.c                                      :+:      :+:    :+:   */
+/*   ft_treat_x.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/07 14:48:35 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/07/24 15:43:31 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/07/24 15:45:36 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
+
 void	conv_x(va_list *list, t_struct *strct)
 {
 	unsigned int nbr;
@@ -27,9 +27,3 @@ void	conv_capsx(va_list *list, t_struct *strct)
 	nbr = va_arg(*list, unsigned int);
 	ft_puthexcapsx(nbr, strct);
 }	
-
-void	ft_printhexa()
-{
-
-	
-}
