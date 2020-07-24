fed35efcf316483da1b8f5bb8c5f274028a50259
Lucas WEGLARZ <lweglarz@e1r5p8.42.fr>
2020-07-24 15:32:25 +0200

    ft_treat_percent.c norme

--- ft_treat_percent.c
+++ ft_treat_percent.c
@@ -6,7 +6,7 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/07 14:49:40 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/07/23 15:11:03 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/07/24 15:06:02 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	conv_per(va_list *list, t_struct *strct)
 {
 	(void)list;
 	if (strct->minus == 0)
-		ft_putwidth(strct, strct->width, 1);
+		ft_putwidth(strct->width, 1, strct);
 	ft_putchar('%', strct);
 	if (strct->minus == 1)
-		ft_putwidth(strct,strct->width, 1);
+		ft_putwidth(strct->width, 1, strct);
 }
