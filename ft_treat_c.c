2272a4ab8f39ac90ed066947aa957b69a90dc037
Lucas WEGLARZ <lweglarz@e1r5p8.42.fr>
2020-07-24 15:28:18 +0200

    ft_treat_c.c norme

--- ft_treat_c.c
+++ ft_treat_c.c
@@ -6,27 +6,26 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/07 14:44:40 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/07/23 12:40:58 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/07/24 15:26:31 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
-void	conv_c(va_list *list, t_struct *strct)
+void		conv_c(va_list *list, t_struct *strct)
 {
 	char c;
 
 	c = va_arg(*list, int);
-	print_char(c, strct);
+	ft_print_char(c, strct);
 }
 
-
-void    print_char(char c, t_struct *strct)
+void		ft_print_char(char c, t_struct *strct)
 {
 	if (strct->minus == 0)
-		ft_putwidth(strct, strct->width, 1);
+		ft_putwidth(strct->width, 1, strct);
 	ft_putchar(c, strct);
-	if(strct-> minus == 1)
-		ft_putwidth(strct, strct->width,  1);;
+	if (strct->minus == 1)
+		ft_putwidth(strct->width, 1, strct);
 }
