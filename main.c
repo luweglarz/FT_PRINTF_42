728866f5624edfc974328f5d1a7f43180fe08212
Lucas WEGLARZ <lweglarz@e1r5p8.42.fr>
2020-07-24 15:35:53 +0200

    update main tester

--- main.c
+++ main.c
@@ -6,7 +6,8 @@ int main()
 	//ft_printf("\\!/%15c\\!/ \n", 'c');
     //res2 = printf("myyy printf c : %%-.00s \n", "hi low");
     //ft_printf("%d");
-    ft_printf("%p");
+    ft_printf("%0-3.7i\n", 3267);
+    printf("%0-3.7i", 3267);
    // printf("%d\n", res1);
     //printf("%d", res2);
 }
